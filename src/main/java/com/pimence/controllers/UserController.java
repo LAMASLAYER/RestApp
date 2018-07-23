662b5b652c77119255ebbfdaf730282c896f45d9
ku714 <jerome.wilmart@clearstream.com>
2018-07-23 14:34:38 +0200

    improving list

--- src/main/java/com/pimence/controllers/UserController.java
+++ src/main/java/com/pimence/controllers/UserController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class UserController {
@@ -14,14 +17,14 @@ public class UserController {
 
     @CrossOrigin
     @RequestMapping("/users")
-    public User[] getUser() {
-        User [] users = new User[5];
+    public List<User> getUser() {
+        List<User> users = new ArrayList<User>();
         LOGGER.info("Retrieving Users !");
-        users[0] = new User(1, "Jerome","1234");
-        users[1] = new User(2,"Zambla","1234");
-        users[2] = new User(3,"Licorne","1234");
-        users[3] =  new User(4,"MrGameAndWatch","1234");
-        users[4] =  new User(5,"Batman","1234");
+        users.add(0, new User(1, "Jerome","1234"));
+        users.add(1, new User(2, "Zambla", "1234"));
+        users.add(2, new User(3, "Licorne", "1234"));
+        users.add(3, new User(4, "MrGameAndWatch", "1234"));
+        users.add(4, new User(5, "Batman", "1234"));
         return users;
     }
 
